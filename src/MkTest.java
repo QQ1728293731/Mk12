b82ecfc04be617c3bcd1c81cb0e98ad5c864191f
邹上豪 <zoush@kean.edu>
2023-04-24 12:47:33 +0800

    *

--- src/MkTest.java
+++ src/MkTest.java
@@ -1,6 +1,12 @@
 //此类用于各种疑难理论测试，成功后可使用branch功能进行实际测试
-//现在正在测试：
+//现在正在测试：清屏
+
+import java.io.IOException;
+import java.util.Scanner;
 
 public class MkTest {
-
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("666");
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+    }
 }

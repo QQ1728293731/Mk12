529cd79050e04e2d812a3834ae73f4149f62d2f5
邹上豪 <zoush@kean.edu>
2023-04-25 13:03:05 +0800

    *

--- src/MkTCPClient.java
+++ src/MkTCPClient.java
@@ -5,9 +5,9 @@ import java.net.Socket;
 //此类使用TCP协议进行数据发送
 public class MkTCPClient {
     public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("10.93.147.208", 33333);
+        Socket socket = new Socket("10.8.29.218", 33333);
         OutputStream os = socket.getOutputStream();
-        os.write("My name is Apermesa".getBytes());
+        os.write("苟利国家生死以，岂因祸福避趋之？Window正在更新，请不要关闭计算机。".getBytes());
         os.close();
     }
 }

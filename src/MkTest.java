e44a3cf556b77bfe161becede86c313063294ccf
邹上豪 <zoush@kean.edu>
2023-05-09 12:15:43 +0800

    Update MkTest.java

--- src/MkTest.java
+++ src/MkTest.java
@@ -1,13 +1,13 @@
 //此类用于各种疑难理论测试，成功后可使用branch功能进行实际测试
 //现在正在测试：网络编程
 
+import java.io.File;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 public class MkTest {
     public static void main(String[] args) throws UnknownHostException {
-        InetAddress ip = InetAddress.getByName("Apermesa");
-        System.out.println(ip.getHostAddress());
-        System.out.println(ip.getHostName());
+       
     }
+
 }
